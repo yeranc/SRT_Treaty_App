c28c8754c71f371ac1c8570912f67d96fd1bdef4
chandana.yeranagu@msg-global.com <chandana.yeranagu@msg-global.com>
2026-03-11 07:35:25 +0000

    New addings

---
+++ zsrt_treaty/webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/m/MessageToast"
+], function(MessageToast) {
+    'use strict';
+
+    return {
+        get_result: function(oEvent) {
+            
+        }
+    }
+});
